3fc5a5dbc1f79b103667bbb634129c7f4d991a76
Jigsxa <max.faust@smail.th-koeln.de>
2022-11-22 13:17:29 +0100

    licht implementiert
    Co-authored-by: Samuel <samuel.schneider@smail.th-koeln.de>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// Pins entsprechen dem layout auf dem ESP8266
+#define LDR A0
+#define led D3
+const int DELAY = 500;
+const int LED_THRESHOLD = 300;
+
+void setup() 
+{
+    Serial.begin(115200); // Serielle Verbindung starten
+    pinMode(led,OUTPUT); // Die LED an Digital 3 als AUSGANG eingestellt
+    delay(DELAY);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+    int sensorValue = analogRead(LDR);
+    if (sensorValue < LED_THRESHOLD)
+    {
+        digitalWrite(led, HIGH);
+    }
+    else
+    {
+        digitalWrite(led, LOW);
+    }
+    Serial.print("Debug: Light value is ");
+    Serial.print(sensorValue);
+    Serial.print("\n"); 
+    delay(DELAY); 
 }
